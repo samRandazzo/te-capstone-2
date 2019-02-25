6c5f6675c91b04782575a70ce404615bd5516b3a
Ean Hibian <eanhibian@gmail.com>
2019-02-25 13:21:33 -0500

    updated.

--- src/test/java/com/techelevator/DAOIntegrationTestSite.java
+++ src/test/java/com/techelevator/DAOIntegrationTestSite.java
@@ -2,16 +2,46 @@ package com.techelevator;
 
 import static org.junit.Assert.assertEquals;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 public class DAOIntegrationTestSite extends DAOIntegrationTest{
 	private JDBCSiteDAO siteDAO;
 	
 	
+private static SingleConnectionDataSource dataSource;
 	
+	@BeforeClass
+	public static void setupDataSource() {
+		dataSource = new SingleConnectionDataSource();
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/campground");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("postgres1");
+		dataSource.setAutoCommit(false);
+	}
+	
+	@AfterClass
+	public static void closeDataSource() throws SQLException {
+		dataSource.destroy();
+	}
+
+	@After
+	public void rollback() throws SQLException {
+		dataSource.getConnection().rollback();
+	}
+	
+	protected DataSource getDataSource() {
+		return dataSource;
+	}
 	@Before
 	public void setup() {
 		
@@ -22,10 +52,6 @@ public class DAOIntegrationTestSite extends DAOIntegrationTest{
 		
 	}
 	
-	@Test
-	public void return_all_employees() {
-		List<Reservation> results = siteDAO.createsite();
-		assertEquals(13, results.size());
-	}
+
 
 }
